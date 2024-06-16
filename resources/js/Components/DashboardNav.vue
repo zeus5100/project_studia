172655116e6500968879bdc1fdfaab3db1d935d9
zeus5100 <dominik1213141555@gmail.com>
2024-06-16 20:23:42 +0200

    add responsive nav menu,

---
+++ resources/js/Components/DashboardNav.vue
@@ -0,0 +1,29 @@
+<script setup>
+import NavLink from '@/Components/NavLink.vue';
+</script>
+
+<template>
+  <NavLink
+    :href="route('admin.dashboard')"
+    :active="route().current('admin.dashboard')"
+  >
+    Panel Główny
+  </NavLink>
+  <NavLink :href="route('register')" :active="route().current('register')">
+    Rejestracja użytkownika
+  </NavLink>
+
+  <NavLink :href="route('event')" :active="route().current('event')">
+    Rejestracja wydarzenia
+  </NavLink>
+
+  <NavLink :href="route('activity')" :active="route().current('activity')">
+    Tworzenie zajęć
+  </NavLink>
+  <NavLink
+    :href="route('admin.manage')"
+    :active="route().current('admin.manage')"
+  >
+    Zarządzaj
+  </NavLink>
+</template>
---
+++ resources/js/Components/ResponsiveDashboardNav.vue
@@ -0,0 +1,35 @@
+<script setup>
+import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+</script>
+
+<template>
+  <ResponsiveNavLink
+    :href="route('admin.dashboard')"
+    :active="route().current('admin.dashboard')"
+  >
+    Panel Główny
+  </ResponsiveNavLink>
+  <ResponsiveNavLink
+    :href="route('register')"
+    :active="route().current('register')"
+  >
+    Rejestracja użytkownika
+  </ResponsiveNavLink>
+
+  <ResponsiveNavLink :href="route('event')" :active="route().current('event')">
+    Rejestracja wydarzenia
+  </ResponsiveNavLink>
+
+  <ResponsiveNavLink
+    :href="route('activity')"
+    :active="route().current('activity')"
+  >
+    Tworzenie zajęć
+  </ResponsiveNavLink>
+  <ResponsiveNavLink
+    :href="route('admin.manage')"
+    :active="route().current('admin.manage')"
+  >
+    Zarządzaj
+  </ResponsiveNavLink>
+</template>
